1e5999391bbaa4742347f5a445ea36acf7d531b1
Prasanna Vijayan <prasanna@shippable.com>
2017-12-10 21:12:52 +0530

    Initial Push - Deployment

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import HomeContainer from './Containers/HomeContainer.js';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <HomeContainer />
+    );
+  }
+}
+
+export default App;
---
+++ src/Containers/HomeContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Header from './../Components/Header/Header.js';
+
+class HomeContainer extends Component {
+  render() {
+    return (
+      <Header />
+    );
+  }
+}
+
+export default HomeContainer;

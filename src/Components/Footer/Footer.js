d32375e25ac1388634a4a2d40fcb40e1a8715f31
Prasanna Vijayan <prasanna@shippable.com>
2018-03-28 13:31:25 +0530

    Refactor for Footer and workshop

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -7,96 +7,131 @@ import { Link } from 'react-router-dom';
 // import FaTwitter from 'react-icons/lib/fa/twitter';
 
 class Footer extends Component {
+
+	copyRightsText() {
+		return (
+			<div className="container d-flex justify-content-between">
+				<div className="text-light-black">
+					<p><Link to="/" className="nostyle">Google Developers Group Madurai</Link></p>
+					<p className="text-light-black">Copyrights &copy; 2018</p>
+				</div>
+				<div className="social_links">
+				</div>
+			</div>
+		)
+	}
+
+	programsLink() {
+		return (
+			<div>
+				<h6 className="text-light-black">Programs</h6>
+				<ul>
+					<li>
+						<a href="https://developers.google.com/programs/community/gdg" rel="noopener noreferrer" target="_blank">Google Developers Group</a>
+					</li>
+					<li>
+						<a href="https://www.womentechmakers.com/" rel="noopener noreferrer" target="_blank">Women Techmakers</a>
+					</li>
+					<li>
+						<a href="https://developers.google.com/startups/" rel="noopener noreferrer" target="_blank">Starup Launchpad</a>
+					</li>
+					<li>
+						<a href="https://developers.google.com/experts/" rel="noopener noreferrer" target="_blank">Google Developers Expert</a>
+					</li>
+				</ul>
+			</div>
+		)
+	}
+
+	techLinks() {
+		return (
+			<div>
+				<h6 className="text-light-black">New Technologies</h6>
+				<ul>
+					<li>
+						<a href="https://www.tensorflow.org/" rel="noopener noreferrer" target="_blank">Tensor Flow</a>
+					</li>
+					<li>
+						<a href="https://developer.android.com/google/index.html" rel="noopener noreferrer" target="_blank">Android</a>
+					</li>
+					<li>
+						<a href="https://developers.google.com/web/progressive-web-apps/" rel="noopener noreferrer" target="_blank">PWA</a>
+					</li>
+					<li>
+						<a href="https://research.google.com/teams/brain/robotics/" rel="noopener noreferrer" target="_blank">Research @ Google</a>
+					</li>
+				</ul>
+			</div>
+		)
+	}
+
+	quickLinks() {
+		return (
+			<div>
+				<h6 className="text-light-black">Quick Links</h6>
+				<ul>
+					<li>
+						<a href="/faq" rel="noopener noreferrer" target="_blank">FAQ</a>
+					</li>
+					<li>
+						<a href="/blog" rel="noopener noreferrer" target="_blank">Blog (WIP)</a>
+					</li>
+					<li>
+						<a href="/coc" rel="noopener noreferrer" target="_blank">Code of Conduct (WIP)</a>
+					</li>
+				</ul>
+			</div>
+		)
+	}
+
+	eventsLinks() {
+		return (
+			<div>
+				<h6 className="text-light-black">Events</h6>
+				<ul>
+					<li>
+						<a href="https://www.meetup.com/GDG-Madurai/" rel="noopener noreferrer" target="_blank">Meetup</a>
+					</li>
+				</ul>
+			</div>
+		)
+	}
+
+	socialLinks() {
+		return (
+			<div>
+				<h6 className="text-light-black">Connect</h6>
+				<ul>
+					<li>
+						<a href="https://plus.google.com/communities/113213079482851146474" rel="noopener noreferrer" target="_blank">
+							Google
+						</a>
+					</li>
+					<li>
+						<a href="https://www.facebook.com/pg/gdgMaduraipage/posts/" rel="noopener noreferrer" target="_blank">
+							Facebook
+						</a>
+					</li>
+					<li>
+						<a href="https://twitter.com/gdgmadurai" rel="noopener noreferrer" target="_blank">
+							Twitter
+						</a>
+					</li>
+				</ul>
+			</div>
+		)
+	}
+
   render() {
     return (
 			<footer className="bg-dark">
-				<div className="container d-flex justify-content-between">
-					<div className="text-light-black">
-						<p><Link to="/" className="nostyle">Google Developers Group Madurai</Link></p>
-						<p className="text-light-black">Copyrights &copy; 2018</p>
-					</div>
-					<div className="social_links">
-					</div>
-				</div>
+				{this.copyRightsText()}
 				<div className="quick_links d-flex justify-content-between">
-					<div>
-						<h6 className="text-light-black">Programs</h6>
-						<ul>
-							<li>
-								<a href="https://developers.google.com/programs/community/gdg" rel="noopener noreferrer" target="_blank">Google Developers Group</a>
-							</li>
-							<li>
-								<a href="https://www.womentechmakers.com/" rel="noopener noreferrer" target="_blank">Women Techmakers</a>
-							</li>
-							<li>
-								<a href="https://developers.google.com/startups/" rel="noopener noreferrer" target="_blank">Starup Launchpad</a>
-							</li>
-							<li>
-								<a href="https://developers.google.com/experts/" rel="noopener noreferrer" target="_blank">Google Developers Expert</a>
-							</li>
-						</ul>
-					</div>
-
-					<div>
-						<h6 className="text-light-black">New Technologies</h6>
-						<ul>
-							<li>
-								<a href="https://www.tensorflow.org/" rel="noopener noreferrer" target="_blank">Tensor Flow</a>
-							</li>
-							<li>
-								<a href="https://developer.android.com/google/index.html" rel="noopener noreferrer" target="_blank">Android</a>
-							</li>
-							<li>
-								<a href="https://developers.google.com/web/progressive-web-apps/" rel="noopener noreferrer" target="_blank">PWA</a>
-							</li>
-							<li>
-								<a href="https://research.google.com/teams/brain/robotics/" rel="noopener noreferrer" target="_blank">Research @ Google</a>
-							</li>
-						</ul>
-					</div>
-
-					<div>
-						<h6 className="text-light-black">Quick Links</h6>
-						<ul>
-							<li>
-								<a href="/faq" rel="noopener noreferrer" target="_blank">FAQ</a>
-							</li>
-							<li>
-								<a href="/blog" rel="noopener noreferrer" target="_blank">Blog (WIP)</a>
-							</li>
-							<li>
-								<a href="/coc" rel="noopener noreferrer" target="_blank">Code of Conduct (WIP)</a>
-							</li>
-						</ul>
-					</div>
-					<div>
-						<h6 className="text-light-black">Events</h6>
-						<ul>
-							<li>
-								<a href="https://www.meetup.com/GDG-Madurai/" rel="noopener noreferrer" target="_blank">Meetup</a>
-							</li>
-						</ul>
-					</div>
-					<div>
-						<h6 className="text-light-black">Connect</h6>
-						<ul>
-							<li>
-								<a href="https://plus.google.com/communities/113213079482851146474" rel="noopener noreferrer" target="_blank">
-									Google
-								</a>
-							</li>
-							<li>
-								<a href="https://www.facebook.com/pg/gdgMaduraipage/posts/" rel="noopener noreferrer" target="_blank">
-									Facebook
-								</a>
-							</li>
-							<li>
-								<a href="https://twitter.com/gdgmadurai" rel="noopener noreferrer" target="_blank">
-									Twitter
-								</a>
-							</li>
-						</ul>
-					</div>
+					{this.programsLink()}
+					{this.techLinks()}
+					{this.quickLinks()}
+					{this.eventsLinks()}
+					{this.socialLinks()}
 				</div>
 			</footer>
     );
--- src/Components/WorkshopCards/WorkshopCards.js
+++ src/Components/WorkshopCards/WorkshopCards.js
@@ -9,76 +9,113 @@ import PaperPlane from 'react-icons/lib/fa/paper-plane-o';
 // import { Link } from 'react-router-dom';
 
 class WorkshopCards extends Component {
+
+	vDevfest() {
+		return (
+			<div className="col-md-4">
+				<div className="card dark-theme">
+					<div className="card-center">
+						<BullHorn className="card-image"/>
+					</div>
+				  <div className="card-block">
+				    <h4 className="card-title">Google DevFest</h4>
+				    <p className="card-text">DevFests are large, community-run events that offers speaker sessions across multiple product areas, hack-a-thons, code labs, and more. Awesome speakers and lot's of fun!</p>
+				  </div>
+				</div>
+			</div>
+		)
+	}
+
+	vWorkshop() {
+		return (
+			<div className="col-md-4">
+				<div className="card dark-theme">
+					<div className="card-center">
+						<LightBulb className="card-image"/>
+					</div>
+				  <div className="card-block">
+				    <h4 className="card-title">Workshop</h4>
+				    <p className="card-text">Tons of workshops, hands-on experience and hacking. Come and learn your favourite technology!</p>
+				  </div>
+				</div>
+			</div>
+		)
+	}
+
+	vMeetups() {
+		return (
+			<div className="col-md-4">
+				<div className="card dark-theme">
+					<div className="card-center">
+						<LocationArrow className="card-image"/>
+					</div>
+				  <div className="card-block">
+				    <h4 className="card-title">Meetups</h4>
+				    <p className="card-text">Our meetups range from large gatherings with demos and lectures, to events like code sprints and hackathons.</p>
+				  </div>
+				</div>
+			</div>
+		)
+	}
+
+	vAndroid() {
+		return (
+			<div className="col-md-4">
+				<div className="card dark-theme">
+					<div className="card-center">
+						<Android className="card-image"/>
+					</div>
+				  <div className="card-block">
+				    <h4 className="card-title">Android</h4>
+				    <p className="card-text">Android is a mobile operating system (OS) based on the Linux kernel and currently developed by Google.</p>
+				  </div>
+				</div>
+			</div>
+		)
+	}
+
+	vVisits() {
+		return (
+			<div className="col-md-4">
+				<div className="card dark-theme">
+					<div className="card-center">
+						<PaperPlane className="card-image"/>
+					</div>
+				  <div className="card-block">
+				    <h4 className="card-title">Industrial visits</h4>
+				    <p className="card-text">Members of the Google Developer Group gets to visit offices of MNCs and startups for wide exposure of real world functioning.</p>
+				  </div>
+				</div>
+			</div>
+		)
+	}
+
+	vCloud() {
+		return (
+			<div className="col-md-4">
+				<div className="card dark-theme">
+					<div className="card-center">
+						<Cloud className="card-image"/>
+					</div>
+				  <div className="card-block">
+				    <h4 className="card-title">Google Cloud</h4>
+				    <p className="card-text">Google Cloud Platform is a cloud computing platform by Google that offers hosting on the same supporting infrastructure that Google uses internally</p>
+				  </div>
+				</div>
+			</div>
+		)
+	}
+
   render() {
     return (
 			<div className="container workshopcard">
 				<div className="row">
-					<div className="col-md-4">
-						<div className="card dark-theme">
-							<div className="card-center">
-								<BullHorn className="card-image"/>
-							</div>
-						  <div className="card-block">
-						    <h4 className="card-title">Google DevFest</h4>
-						    <p className="card-text">DevFests are large, community-run events that offers speaker sessions across multiple product areas, hack-a-thons, code labs, and more. Awesome speakers and lot's of fun!</p>
-						  </div>
-						</div>
-					</div>
-					<div className="col-md-4">
-						<div className="card dark-theme">
-							<div className="card-center">
-								<LightBulb className="card-image"/>
-							</div>
-						  <div className="card-block">
-						    <h4 className="card-title">Workshop</h4>
-						    <p className="card-text">Tons of workshops, hands-on experience and hacking. Come and learn your favourite technology!</p>
-						  </div>
-						</div>
-					</div>
-					<div className="col-md-4">
-						<div className="card dark-theme">
-							<div className="card-center">
-								<LocationArrow className="card-image"/>
-							</div>
-						  <div className="card-block">
-						    <h4 className="card-title">Meetups</h4>
-						    <p className="card-text">Our meetups range from large gatherings with demos and lectures, to events like code sprints and hackathons.</p>
-						  </div>
-						</div>
-					</div>
-					<div className="col-md-4">
-						<div className="card dark-theme">
-							<div className="card-center">
-								<Android className="card-image"/>
-							</div>
-						  <div className="card-block">
-						    <h4 className="card-title">Android</h4>
-						    <p className="card-text">Android is a mobile operating system (OS) based on the Linux kernel and currently developed by Google.</p>
-						  </div>
-						</div>
-					</div>
-					<div className="col-md-4">
-						<div className="card dark-theme">
-							<div className="card-center">
-								<PaperPlane className="card-image"/>
-							</div>
-						  <div className="card-block">
-						    <h4 className="card-title">Industrial visits</h4>
-						    <p className="card-text">Members of the Google Developer Group gets to visit offices of MNCs and startups for wide exposure of real world functioning.</p>
-						  </div>
-						</div>
-					</div>
-					<div className="col-md-4">
-						<div className="card dark-theme">
-							<div className="card-center">
-								<Cloud className="card-image"/>
-							</div>
-						  <div className="card-block">
-						    <h4 className="card-title">Google Cloud</h4>
-						    <p className="card-text">Google Cloud Platform is a cloud computing platform by Google that offers hosting on the same supporting infrastructure that Google uses internally</p>
-						  </div>
-						</div>
-					</div>
+					{this.vDevfest()}
+					{this.vWorkshop()}
+					{this.vMeetups()}
+					{this.vAndroid()}
+					{this.vVisits()}
+					{this.vCloud()}
 				</div>
 			</div>
     );
